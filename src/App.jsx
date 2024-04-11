d6f82427f56b0e4fdac0e335b802724c95ad3ba4
phillipSmach95 <smach.phillip@gmail.com>
2024-04-11 09:55:05 +0200

    fjdlks

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,26 @@ import { useState } from "react";
 
 function App() {
   
+  const randomRGB = () => { 
+    return Math.ceil(Math.random()*255);}
+  console.log(randomRGB());
   const data = [
     {title:'red',red:255,green:0,blue:0},
     {title:'green',red:0,green:255,blue:0},
     {title:'blue',red:0,green:0,blue:255},
+    {title:'random',red:randomColors,green:0,blue:255},
+    
   ]
+  
+  const color = 
+    {red:0,green:0,blue:0}
+  
   const [colors, setColors] = useState(data);
-  const randomRGB = () => { 
-    
-    
-    return Math.round(Math.random()*255).toFixed(0);}
-  console.log(randomRGB());
+  const [randomColors, setRandomColors] = useState(color);
+  const setRandomColor = ()=>{
+    setRandomColors({red:randomRGB(),blue:randomRGB(),green:randomRGB()})
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -26,7 +35,7 @@ function App() {
           </ul>
         </div>
         <div>
-
+            <button onClick={setRandomColors()}></button>
         </div>
       </header>
     </div>
