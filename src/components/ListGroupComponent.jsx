ab66388593d435506423ab2fd3332f5ea66ca671
phillipSmach95 <smach.phillip@gmail.com>
2024-04-17 21:08:35 +0200

    two lists

--- src/components/ListGroupComponent.jsx
+++ src/components/ListGroupComponent.jsx
@@ -1,19 +1,49 @@
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
-import './ListGroupComponent.css'
-import { useState } from 'react';
+import { Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import "./ListGroupComponent.css";
+import { useState } from "react";
 
-function ListGroupComponent(){
-    const data = ['One Piece','Demon Slayer', 'HunterXHunter', 'Dragonball', 'made in abyss']
-    const [animeList, setAnimeList] = useState(data)
-    const [isActive, setIsActive] = useState(false)
-    
-    return(
+function ListGroupComponent() {
+  const data = [
+    "One Piece",
+    "Demon Slayer",
+    "HunterXHunter",
+    "Dragonball",
+    "made in abyss",
+  ];
+  const [animeList, setAnimeList] = useState(data);
+
+  return (
     <div>
-        <h1>anime</h1>
-        <ListGroup>
-            {animeList.map((a, i)=>(<ListGroup.Item variant="primary" active={isActive} onClick={()=>{setIsActive(!isActive)}} key={i}>{a}</ListGroup.Item>))}
-        </ListGroup>
+      <h1>anime</h1>
+      <ListGroup>
+        <Row>
+          <Col>
+            {animeList.map((a, i) => (
+              <ListGroup.Item
+                variant="primary"
+                action
+                onClick={() => {}}
+                key={i}
+              >
+                {a}
+              </ListGroup.Item>
+            ))}
+          </Col>
+          <Col>
+            {animeList.map((a, i) => (
+              <ListGroup.Item
+                variant="primary"
+                action
+                onClick={() => {}}
+                key={i}
+              >
+                {a}
+              </ListGroup.Item>
+            ))}
+          </Col>
+        </Row>
+      </ListGroup>
     </div>
-)
+  );
 }
 export default ListGroupComponent;
