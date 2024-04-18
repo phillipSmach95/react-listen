4e22f4df64100336f7f6fa21a25f979272cb8e0b
phillipSmach95 <smach.phillip@gmail.com>
2024-04-18 11:04:19 +0200

    moving list not working

--- src/components/ListGroupComponent.jsx
+++ src/components/ListGroupComponent.jsx
@@ -1,6 +1,7 @@
-import { Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
+import { ButtonGroup, Col, Container, ListGroup, Row } from "react-bootstrap";
 import "./ListGroupComponent.css";
 import { useState } from "react";
+import { Button } from "bootstrap";
 
 function ListGroupComponent() {
   const data = [
@@ -11,38 +12,78 @@ function ListGroupComponent() {
     "made in abyss",
   ];
   const [animeList, setAnimeList] = useState(data);
+  const [secondAnimeList, setSecondAnimeList] = useState([]);
+  const [index, setIndex] = useState(0);
+
+  const moveItemToSecondList = () => {
+    setSecondAnimeList([...secondAnimeList, animeList[index]]);
+    setAnimeList(animeList.filter((a) => a !== animeList[index]));
+  };
+  const moveItemToAnimeList = () => {
+    setAnimeList([...animeList, secondAnimeList[index]]);
+    setSecondAnimeList(
+      secondAnimeList.filter((a) => a !== secondAnimeList[index])
+    );
+  };
+  const moveAllToAnimeList = () =>{
+    setAnimeList(...animeList, secondAnimeList)
+    setSecondAnimeList([])
+  }
+  const moveAllToSecondAnimeList = () =>{
+    setSecondAnimeList(...secondAnimeList, animeList)
+    setAnimeList([])
+  }
 
   return (
     <div>
       <h1>anime</h1>
-      <ListGroup>
+
+      <Container>
         <Row>
           <Col>
-            {animeList.map((a, i) => (
-              <ListGroup.Item
-                variant="primary"
-                action
-                onClick={() => {}}
-                key={i}
-              >
-                {a}
-              </ListGroup.Item>
-            ))}
+            <ListGroup>
+              {animeList.map((a, i) => (
+                <ListGroup.Item
+                  variant="primary"
+                  action
+                  onClick={() => {
+                    setIndex(i);
+                  }}
+                  key={i}
+                >
+                  {a}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
           </Col>
           <Col>
-            {animeList.map((a, i) => (
-              <ListGroup.Item
-                variant="primary"
-                action
-                onClick={() => {}}
-                key={i}
-              >
-                {a}
-              </ListGroup.Item>
-            ))}
+            <ButtonGroup>
+              <Row>
+                <button onClick={()=> moveAllToSecondAnimeList()}>{">>"}</button>
+                <button onClick={() => moveItemToSecondList()}>{">"}</button>
+                <button onClick={()=> moveItemToAnimeList()}>{"<"}</button>
+                <button onClick={()=> moveAllToAnimeList()}>{"<<"}</button>
+              </Row>
+            </ButtonGroup>
+          </Col>
+          <Col>
+            <ListGroup>
+              {secondAnimeList.map((a, i) => (
+                <ListGroup.Item
+                  variant="primary"
+                  action
+                  onClick={() => {
+                    setIndex(i);
+                  }}
+                  key={i}
+                >
+                  {a}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
           </Col>
         </Row>
-      </ListGroup>
+      </Container>
     </div>
   );
 }
